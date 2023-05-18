abfd8b240c6f2eb71248e1ad348798dcad7c4176
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2023-05-18 18:16:29 -0300

    feat: transactions container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,5 +4,12 @@ import './providers';
 
 import { IUserRepository } from '@modules/User/repositories/UserRepository.interface';
 import { UserRepository } from '@modules/User/repositories/UserRepository';
+import { ITransactionsRepository } from '@modules/Transactions/repositories/TransactionsRepository.interface';
+import { TransactionsRepository } from '@modules/Transactions/repositories/TransactionsRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<ITransactionsRepository>(
+  'TransactionsRepository',
+  TransactionsRepository,
+);
