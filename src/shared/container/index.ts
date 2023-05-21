1a1f444c17595b03558e3a10fc964cc98ead4fda
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2023-05-21 13:50:15 -0300

    feat: goals/notifications container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,10 +6,24 @@ import { IUserRepository } from '@modules/User/repositories/UserRepository.inter
 import { UserRepository } from '@modules/User/repositories/UserRepository';
 import { ITransactionsRepository } from '@modules/Transactions/repositories/TransactionsRepository.interface';
 import { TransactionsRepository } from '@modules/Transactions/repositories/TransactionsRepository';
+import { IGoalsRepository } from '@modules/Goals/repositories/GoalsRepository.interface';
+import { GoalsRepository } from '@modules/Goals/repositories/GoalsRepository';
+import { NotificationsRepository } from '@modules/Notifications/repositories/NotificationsRepository';
+import { INotificationsRepository } from '@modules/Notifications/repositories/NotificationsRepositories.interface';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 
 container.registerSingleton<ITransactionsRepository>(
   'TransactionsRepository',
   TransactionsRepository,
+);
+
+container.registerSingleton<IGoalsRepository>(
+  'GoalsRepository',
+  GoalsRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
 );
