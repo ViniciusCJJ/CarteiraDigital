04e6b75400fde65afeefeef59ae3c96be75f3762
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2023-05-21 13:47:53 -0300

    feat: notifications

--- src/modules/User/services/CreateSession.service.ts
+++ src/modules/User/services/CreateSession.service.ts
@@ -8,6 +8,10 @@ import { refreshToken_config } from '@config/auth';
 import { jwtGenerate } from '@shared/util/jwtGenerate';
 import { UserRole } from '@prisma/client';
 import { plainToInstance } from 'class-transformer';
+import { ITransactionsRepository } from '@modules/Transactions/repositories/TransactionsRepository.interface';
+import { INotificationsRepository } from '@modules/Notifications/repositories/NotificationsRepositories.interface';
+import { IGoalsRepository } from '@modules/Goals/repositories/GoalsRepository.interface';
+import { dateDiff } from '@shared/util/dateDiff';
 import { IUserRepository } from '../repositories/UserRepository.interface';
 import {
   ICreateSessionDTO,
@@ -20,6 +24,15 @@ class CreateSessionService {
   constructor(
     @inject('UserRepository')
     private userRepository: IUserRepository,
+
+    @inject('TransactionsRepository')
+    private transactionsRepository: ITransactionsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
+
+    @inject('GoalsRepository')
+    private goalsRepository: IGoalsRepository,
 
     @inject('RedisProvider')
     private redisProvider: IRedisProvider,
@@ -57,6 +70,52 @@ class CreateSessionService {
 
       await this.userRepository.update(user);
     }
+
+    user.avatar = `${process.env.APP_API_URL}files/${user.avatar}`;
+
+    const transactions = await this.transactionsRepository.listAll({
+      filters: { user_id: user.id },
+    });
+
+    const balance = transactions.results.reduce(
+      (acc, transaction) =>
+        transaction.type === 'Income'
+          ? acc + transaction.value
+          : acc - transaction.value,
+      0,
+    );
+
+    if (balance < 0) {
+      await this.notificationsRepository.create({
+        title: 'Saldo negativo',
+        text: `Seu saldo está negativo em R$ ${balance}`,
+        user_id: user.id,
+      });
+    }
+
+    const goals = await this.goalsRepository.listAll({
+      filters: { user_id: user.id },
+    });
+    goals.results.forEach(async goal => {
+      const now = new Date();
+      const diff = dateDiff(goal.final_date, now);
+      if (diff <= 7) {
+        const alreadyNotified = await this.notificationsRepository.findBy({
+          goal_id: goal.id,
+        });
+
+        if (!alreadyNotified) {
+          this.notificationsRepository.create({
+            title: 'Meta próxima do fim',
+            text: `Sua meta ${goal.title} está próxima do fim, faltam: R$ ${
+              goal.value - goal.total_raised
+            } para arrecadar`,
+            goal_id: goal.id,
+            user_id: user.id,
+          });
+        }
+      }
+    });
 
     return {
       user: plainToInstance(User, user),
